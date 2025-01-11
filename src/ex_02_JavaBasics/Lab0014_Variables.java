7a75e958333f27eb8602c7aacf8e8ade4c6f69b5
Premalak <premala.anand@bigbasket.com>
2025-01-11 21:07:35 +0530

    11th jan  java programs

---
+++ src/ex_02_JavaBasics/Lab0014_Variables.java
@@ -0,0 +1,18 @@
+package ex_02_JavaBasics;
+
+public class Lab0014_Variables {
+    public static void main(String[] args) {
+        int a = 20;
+      int b = 35;
+       b = 35 + 1;
+       int c = 75 ;
+       int age = 65;
+       age = 89;
+
+
+        // datatype variable_name = variable_value
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(age);
+    }
+}
